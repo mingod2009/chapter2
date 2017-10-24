ff85d50b3bb5398143736691081890e541e38220
niems <mingod@qq.com>
2017-10-24 10:03:21 +0800

    no message

---
+++ src/main/java/com/smart4j/chapter2/controller/CustomerServlet.java
@@ -0,0 +1,38 @@
+package com.smart4j.chapter2.controller;
+
+import com.smart4j.chapter2.model.Customer;
+import com.smart4j.chapter2.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by sam on 2017/10/23.
+ */
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
+
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
